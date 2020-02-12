854a4884a691abc0f83a10f99366cd3c3c315f95
Theodor Forgaard <tjforgaa@stud.ntnu.no>
2020-02-12 14:18:06 +0100

    fixed elevator.h

--- source/main.c
+++ source/main.c
@@ -10,10 +10,10 @@ int main(){
         fprintf(stderr, "Unable to initialize hardware\n");
         exit(1);
     }
-
+    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
     Elevator_initialize();
     while(1){
-        
+
         if(hardware_read_stop_signal()){
             hardware_command_movement(HARDWARE_MOVEMENT_STOP);
             break;
