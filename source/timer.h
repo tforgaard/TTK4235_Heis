43468a27fa360c67a4d03da7f4e4e7f12e254359
Theodor Forgaard <tjforgaa@stud.ntnu.no>
2020-02-27 22:20:42 +0100

    updated timer documentation

--- source/timer.h
+++ source/timer.h
@@ -1,7 +1,23 @@
+/**
+ * @file
+ * @brief Timer module is a stopwatch.
+ */
+
 #ifndef TIMER_H
 #define TIMER_H
 
+/**
+ * @brief Reset the stopwatch and starts it
+ *
+*/
 void Timer_set();
+
+/**
+ * @brief Returns the time since the timer was set
+ *
+ * @return the time 
+ * 
+*/
 int Timer_get();
 
 #endif
